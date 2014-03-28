a08639c7ad5c23b1e9579f3a5ff789b44a3d71a2
David Andrade <dandrade@agoragames.com>
2014-03-28 09:41:33 -0400

    Add categories to group multiple lines together

--- src/gvizPlus.js
+++ src/gvizPlus.js
@@ -1,31 +1,89 @@
 var gvizPlus = {}
 gvizPlus._construct = function() {
 
-    function DataLine(id) {
+    function Category(name) {
+        this.name = name;
+
+        this.columns = [];
+        this.rows = [];
     }
 
-    function DataSet(name, dataTable) {
-        this.name = name;
-        this.data = dataTable;
+    function DataSet(hAxis) {
+        this.categories = [];
+        this.hAxis = hAxis;
+
+        this.getOrCreateCategoryIndex = function(name) {
+            var index = -1;
+            for(var i = 0; i < this.categories.length; i++) {
+                if(this.categories[i].name === name) {
+                    index = i;
+                    break;
+                }
+            }
+
+            if(index == -1) {
+                index = this.categories.length;
+                this.categories.push(new Category(name));
+            }
+            return index;
+        }
+
+        this.addCategory = function(category, columns, rows) {
+            var index = this.getOrCreateCategoryIndex(category);
+            for(var i = 0; i < columns.length; i++) {
+                this.categories[index].columns.push(columns[i]);
+            }
+            for(var i = 0; i < rows.length; i++) {
+                this.categories[index].rows.push(rows[i]);
+            }
+        }
     }
 
     function Graph(id, title) {
         this.chart = new google.visualization.LineChart(document.getElementById(id));
-        this.dataSets = [];
-        
+
         // Drawing properties
         this.title = title;
         this.hAxisTitle = "";
         this.width = 1000;
         this.height = 500;
 
-        this.addDataSet = function(dataSet) {
-            this.dataSets.push(dataSet);
-        }
+        this.draw = function(dataSet) {
+            var dataTable = new google.visualization.DataTable();
+            dataTable.addColumn(dataSet.hAxis);
+            for (var i = 0; i < dataSet.categories.length; i++) {
+                var category = dataSet.categories[i];
+                var currentColumns = dataTable.getNumberOfColumns() - 1;
 
-        this.draw = function() {
-            this.chart.draw(this.dataSets[0].data, {
+                for(var j = 0; j < category.columns.length; j++) {
+                    dataTable.addColumn(category.columns[j]);
+                }
+
+                for(var j = 0; j < category.rows.length; j++) {
+                    var row = category.rows[j];
+                    if(i == 0) {
+                        // We will take everything as-is from the first dataset
+                        dataTable.addRow(row);
+                    } else {
+                        // For the rest, we'll add empty values until it's our spot
+                        var newRow = [row[0]];
+
+                        for(var k = 0; k < currentColumns; k++) {
+                            newRow.push(null);
+                        }
+
+                        for(var k = 1; k < row.length; k++) {
+                            newRow.push(row[k]);
+                        }
+                        dataTable.addRow(newRow);
+                    }
+                }
+            }
+
+            this.chart.draw(dataTable, {
               title : this.title,
+              legend : 'none', // We'll draw a better legend
+              interpolateNulls : false,
               width: this.width,
               height: this.height,
               vAxis: {0: {}, 1: {}},
